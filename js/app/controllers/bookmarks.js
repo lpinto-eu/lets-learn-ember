0c4f394aa8d3ce632047cf2f032f2f12d3cdf498
lpinto-eu <mail@lpinto.eu>
2013-07-28 21:13:06 +0100

    Lets build an Ember App
    Part 2

--- js/app/controllers/bookmarks.js
+++ js/app/controllers/bookmarks.js
@@ -1,1 +1,17 @@
-App.BookmarksController = Ember.ArrayController.extend();
+App.BookmarksController = Ember.ArrayController.extend({
+	pushSort: function(attribute) {
+		if (this.get("sortProperties.firstObject") == attribute) {
+			this.toggleProperty("sortAscending");
+		} else {
+			this.set("sortProperties", [attribute]);
+		}
+	},
+	
+	favorites: function() {
+		return this.filterProperty("favorite", true);
+	}.property("@each.favorite"),
+	
+	regular: function() {
+		return this.filterProperty("favorite", false);
+	}.property("@each.favorite")
+});
--- js/app/models/bookmark.js
+++ js/app/models/bookmark.js
@@ -1,4 +1,10 @@
 App.Bookmark = DS.Model.extend({
 	title:	DS.attr("string"),
-	url:	DS.attr("string")
+	url:	DS.attr("string"),
+	favorite: DS.attr("boolean"),
+	
+	changeFavorite: function() {
+		this.get("transaction").commit();
+		console.log("transaction was commited.")
+	}.observes("favorite")
 });
